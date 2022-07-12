1f0dedfacf739908056449d335613a1097208d3b
Mark <datawithmark@gmail.com>
2022-07-12 23:34:48 +0530

    Merge Sorted Array

---
+++ LeetCode/LC88.cpp
@@ -0,0 +1,19 @@
+/*
+ * LeetCode. Merge Sorted Array
+ */
+
+class Solution
+{
+public:
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n)
+    {
+        int a = m - 1, b = n - 1, i = m + n - 1;
+        while (b >= 0)
+        {
+            if (a >= 0 && nums1[a] > nums2[b])
+                nums1[i--] = nums1[a--];
+            else
+                nums1[i--] = nums2[b--];
+        }
+    }
+};
