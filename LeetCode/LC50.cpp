04f718deb16b4d1bca1ae29fd0321df00b1210f6
Mark <datawithmark@gmail.com>
2022-07-13 15:37:38 +0530

    Pow(x, n)

---
+++ LeetCode/LC50.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+public:
+    double myPow(double x, int n)
+    {
+        double result = 1;
+        while (n)
+        {
+            if (n % 2)
+                result = n > 0 ? result * x : result / x;
+            x = x * x;
+            n /= 2;
+        }
+        return result;
+    }
+};
